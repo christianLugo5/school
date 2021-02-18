9d395279af5013081cd6c1877d46d7bfbaf3e08d
Christian Lugo <christianrlugo5@gmail.com>
2021-02-18 12:11:45 -0700

    Create Employee model
    added jpa annotations

---
+++ src/main/java/com/school/portal/model/Employee.java
@@ -0,0 +1,50 @@
+package com.school.portal.model;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+enum EducationalAttainment{
+	DOCTORAL, PROFESSIONAL, MASTER, BACHELOR, HIGHSCHOOL;	
+}
+
+@Entity
+public class Employee extends Person {
+	
+	private int salary;
+	@Column(name = "educational_attainment")
+	private EducationalAttainment educationalAttainment;
+	private String identifier;
+	
+	
+	protected Employee() {}
+	
+	public Employee(String name, String lastname, String phoneNumber, Date dateOfBirth, Gender gender, String email,
+			BloodType bloodType, int salary, EducationalAttainment educationalAttainment, String identifier) {
+		super(name, lastname, phoneNumber, dateOfBirth, gender, email, bloodType);
+		this.salary = salary;
+		this.educationalAttainment = educationalAttainment;
+		this.identifier = identifier;
+	}
+
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public EducationalAttainment getEducationalAttainment() {
+		return educationalAttainment;
+	}
+	public void setEducationalAttainment(EducationalAttainment educationalAttainment) {
+		this.educationalAttainment = educationalAttainment;
+	}
+	public String getIdentifier() {
+		return identifier;
+	}
+	public void setIdentifier(String identifier) {
+		this.identifier = identifier;
+	}
+		
+}
